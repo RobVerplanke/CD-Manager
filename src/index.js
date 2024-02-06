4b3194a6efca6a9850b522896c567eafb804dda5
Rob <rverplanke@gmail.com>
2024-02-06 20:16:47 +0100

    begin with layout

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import albumTest from './modules/album.js';
 import cdTest from './modules/cd.js';
 import trackTest from './modules/track.js';
 
-const headerHolder = document.querySelector('#header-holder');
+const logoHolder = document.querySelector('#header-logo-holder');
 const mainHolder = document.querySelector('#main-holder');
-const footerHolder = document.querySelector('#footer-holder');
 
-headerHolder.innerHTML = '<h2>This is nav</h2><br><br>';
+logoHolder.innerHTML = '<h2>CDM</h2>';
 mainHolder.innerHTML = '<h2>This is main section</h2><br>';
 mainHolder.innerHTML += `<h3>${albumTest()}</h3><br>`;
 mainHolder.innerHTML += `<h3>${cdTest()}</h3><br>`;
 mainHolder.innerHTML += `<h3>${trackTest()}</h3><br><br>`;
-footerHolder.innerHTML = '<h2>This is footer</h2>';
